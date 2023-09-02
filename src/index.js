b3236e784fe6122d29e4a490cd80ab80be891139
Ulises Quintanilla <ulisesqg@gmail.com>
2023-09-01 23:01:11 -0700

    Request logger

--- src/index.js
+++ src/index.js
@@ -14,4 +14,21 @@ const error = (data) => {
     _logger('ERROR', data)
 }
 
-export {info, warning, error}
+function requestLogger () {
+    return function _requestLogger (req, res, next) {
+        const oldSend = res.send
+        const _startTime = new Date().getTime()
+
+        res.send = function (data) {
+            const _endTime = new Date().getTime()
+            console.log(
+              `[${new Date().toISOString()}]: ${req.method.toUpperCase()} ${req.originalUrl} ${res.statusCode} ${_endTime - _startTime} ms`
+            )
+
+            oldSend.apply(res, arguments)
+        }
+        next()
+    }
+}
+
+export {info, warning, error, requestLogger}
